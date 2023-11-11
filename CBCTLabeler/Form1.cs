986998f7ecdea7e8ef592a19b272cbaf730437a6
Dancing-Github <1248294871@qq.com>
2023-11-11 17:16:32 +0800

    合并master

--- CBCTLabeler/Form1.cs
+++ CBCTLabeler/Form1.cs
@@ -8,14 +8,21 @@ using System.Windows.Forms;
 using Newtonsoft.Json;
 using static System.Net.Mime.MediaTypeNames;
 using System.Xml.Linq;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace CBCTLabeler
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
